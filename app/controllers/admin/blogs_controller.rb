97c5705cf66be2525186d47fc065ea1fc02bad2d
john joseph <atiquajohn@gmail.com>
2017-07-23 02:08:55 +0700

    code migration to admin complete

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -2,4 +2,34 @@ class Admin::BlogsController < Admin::BaseController
   def index
     @blogs = Blog.all
   end
+
+  def new
+    @blogs = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    redirect_to blogs_path if @blog.save
+  end
+
+  def edit
+    @blogs = Blog.friendly.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.friendly.find(params[:id])
+
+    redirect_to blogs_path if @blog.update_attributes(blog_params)
+  end
+
+  def destroy
+    @blog = Blog.friendly.find(params[:id])
+    redirect_to blogs_path if @blog.destroy
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body, :description)
+  end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,38 +3,8 @@ class BlogsController < ApplicationController
     @blogs = Blog.all
   end
 
-  def new
-    @blogs = Blog.new
-  end
-
-  def create
-    @blog = Blog.new(blog_params)
-    redirect_to blogs_path if @blog.save
-  end
-
   def show
     # @blog = Blog.where(id: params[:id]).first
     @blog = Blog.friendly.find(params[:id])
-  end
-
-  def edit
-    @blogs = Blog.friendly.find(params[:id])
-  end
-
-  def update
-@blog = Blog.where(id: params[:id]).first
-
-    redirect_to blogs_path if @blog.update_attributes(blog_params)
-  end
-
-  def destroy
-    @blog = Blog.where(id: params[:id]).first
-    redirect_to blogs_path if @blog.destroy
-  end
-
-  private
-
-  def blog_params
-    params.require(:blog).permit(:title, :body, :description)
   end
 end
